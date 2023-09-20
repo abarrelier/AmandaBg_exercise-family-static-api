e12d4c43d890090148da90b20e5e0e9245a28987
abarrelier <128666422+abarrelier@users.noreply.github.com>
2023-09-20 08:05:13 +0000

    [Exercise-family-static-api]

--- src/app.py
+++ src/app.py
@@ -25,18 +25,55 @@ def handle_invalid_usage(error):
 def sitemap():
     return generate_sitemap(app)
 
+# GETTING ALL MEMBERS
 @app.route('/members', methods=['GET'])
 def handle_hello():
 
     # this is how you can use the Family datastructure by calling its methods
     members = jackson_family.get_all_members()
-    response_body = {
-        "hello": "world",
-        "family": members
+    return jsonify(members), 200
+
+# GETTING SINGLE MEMBER BY ID
+@app.route('/member/<int:id>', methods=['GET'])
+def get_single_member(id):
+    single_member = jackson_family.get_member(id)
+    response_body ={
+        "member" : single_member
+    }
+    return jsonify(response_body), 200
+
+
+# ADD NEW MEMBER
+@app.route('/member', methods=['POST'])
+def add_member():
+    body = request.get_json()
+    
+    if "first_name" not in body:
+        raise APIException(f"Must provide a name.", status_code=400)
+    if "age" not in body:
+        raise APIException(f"Must provide an age.", status_code=400)
+    id = jackson_family._generateId()
+    if "id" in body:
+        id = body['id']
+
+    new_member = {
+        "id" : id,
+        "first_name" : body.get("first_name"),
+        "last_name" : jackson_family.last_name,
+        "age" : body.get("age"),
+        "lucky_numbers" : body.get("lucky_numbers")
     }
 
+    all_members = jackson_family.add_member(new_member)
+    return jsonify({"done": True}), 200
 
-    return jsonify(response_body), 200
+# DELETE MEMBER
+@app.route('/member/<int:id>', methods=['DELETE'])
+def delete_member(id):
+    jackson_family.delete_member(id)
+    return jsonify({"done": True}), 200
+
+
 
 # this only runs if `$ python src/app.py` is executed
 if __name__ == '__main__':
